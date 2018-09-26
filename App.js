857eae3a781d5fd70b1b2dcde7b3dca81f8717d0
Evan Briolat <briolate@gmail.com>
2018-09-25 22:12:38 -0400

    Moved on to rendering arrays

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableHighlight, Text, View } from 'react-native';
+
+// import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class App extends Component {
-  state = {
-    greeting: 'Welcome to MyProject!'
-  };
-
-  updateGreeting = () => {
-    this.setState({
-      greeting: 'New greeting'
-    });
-  };
-
   render() {
+    const people = [
+      { name: 'Evan', age: '30' },
+      { name: 'Alyssa', age: '28' },
+      { name: 'Russ', age: '31' }
+    ];
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>{this.state.greeting}</Text>
-        <Text onPress={this.updateGreeting}>Change Greeting</Text>
+        {people.map((person, index) => (
+          <Text key={index} style={styles.text}>
+            {person.name} is {person.age}
+          </Text>
+        ))}
       </View>
     );
   }
@@ -25,13 +26,38 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'green'
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+  text: {
+    fontSize: 22
   }
 });
+
+{
+  /* <TouchableHighlight
+  onPress={this.onPress}
+  style={styles.button}
+  underlayColor="rgba(50, 183, 255, .8)"
+>
+  <View style={styles.textContatiner}>
+    <Icon name="home" size={30} color="white" />
+    <Text style={styles.text}>Home</Text>
+  </View>
+</TouchableHighlight>; */
+}
+// button: {
+//   margin: 10,
+//   backgroundColor: 'rgba(50, 183, 255, 1)',
+//   borderRadius: 30,
+//   padding: 17
+// },
+// textContatiner: {
+//   flexDirection: 'row',
+//   justifyContent: 'center',
+//   alignItems: 'center'
+// },
+// text: {
+//   marginLeft: 9,
+//   color: 'white',
+//   fontSize: 20
+// }
