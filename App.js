845a280afd0850bf46557670e04f07f8e0fcae0c
Evan <briolate@gmail.com>
2018-09-26 13:28:41 -0400

    Introduced FlatList and pagination refreshing with fetch api

--- App.js
+++ App.js
@@ -1,23 +1,66 @@
 import React, { Component } from 'react';
-import { StyleSheet, TouchableHighlight, Text, View } from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+  View
+} from 'react-native';
 
 // import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class App extends Component {
-  render() {
-    const people = [
-      { name: 'Evan', age: '30' },
-      { name: 'Alyssa', age: '28' },
-      { name: 'Russ', age: '31' }
-    ];
+  state = {
+    people: [],
+    refreshing: false,
+    page: 1
+  };
 
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    this.setState({ refreshing: true });
+    try {
+      const data = await fetch(
+        `https://swapi.co/api/people?page=${this.state.page}`
+      );
+      const json = await data.json();
+      this.setState({
+        people: json.results,
+        page: this.state.page + 1,
+        refreshing: false
+      });
+    } catch (err) {
+      console.log('error fetching data: ', err);
+    }
+  };
+
+  renderItem = ({ item }) => {
+    return (
+      <View
+        style={{ padding: 15, borderBottomColor: '#ddd', borderBottomWidth: 1 }}
+      >
+        <Text style={{ fontSize: 20 }}>{item.name}</Text>
+        <Text style={{ color: 'rgba(0, 0, 0, .5)' }}>
+          Gender: {item.gender}
+        </Text>
+      </View>
+    );
+  };
+
+  render() {
     return (
       <View style={styles.container}>
-        {people.map((person, index) => (
-          <Text key={index} style={styles.text}>
-            {person.name} is {person.age}
-          </Text>
-        ))}
+        <Text>People:</Text>
+        <FlatList
+          onRefresh={this.fetchData}
+          refreshing={this.state.refreshing}
+          data={this.state.people}
+          keyExtractor={item => item.name}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
@@ -26,7 +69,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingVertical: 30,
+    paddingHorizontal: 14
   },
   text: {
     fontSize: 22
